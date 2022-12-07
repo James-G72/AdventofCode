831c024a953cb2a1b30ccfd6ab4f8e56ba88aa9a
James Gower <66065325+James-G72@users.noreply.github.com>
2022-12-07 01:38:40 +0000

    Day 4 part 2 complete

--- Day4.py
+++ Day4.py
@@ -14,3 +14,21 @@ for pair in pairs:
         p1_count += 1
 
 print("Part 1: "+str(p1_count))
+
+# -------- Part 2 ----------
+p2_count = 0
+for pair in pairs:
+    elves = pair.split(",")
+    if int(elves[0].split("-")[0]) < int(elves[1].split("-")[0]):
+        if int(elves[0].split("-")[1]) >= int(elves[1].split("-")[0]):
+            p2_count += 1
+    elif int(elves[0].split("-")[0]) > int(elves[1].split("-")[0]):
+        if int(elves[0].split("-")[0]) <= int(elves[1].split("-")[1]):
+            p2_count += 1
+    else:
+        p2_count += 1
+
+print("Part 2: "+str(p2_count))
+
+
+
