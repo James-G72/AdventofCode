219a27eac5702b6d776be75faebbb3fee2d9221c
James Gower <66065325+James-G72@users.noreply.github.com>
2022-12-07 23:38:05 +0000

    Day 6 part 2 complete

--- Day6.py
+++ Day6.py
@@ -1,5 +1,11 @@
 stream = open("Day6_Input.txt").read()
 
+# ------- Part 1 ---------
 for char_ind in range(4, len(stream)):
     if len(set(stream[char_ind-4:char_ind])) == 4:
         print("Part 1: "+str(char_ind))
+
+# -------- Part 2 ---------
+for char_ind in range(14, len(stream)):
+    if len(set(stream[char_ind-14:char_ind])) == 14:
+        print("Part 2: "+str(char_ind))
