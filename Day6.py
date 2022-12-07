91e3fd92296471f422a5af7fe2e5c54f38ba7d3e
James Gower <66065325+James-G72@users.noreply.github.com>
2022-12-07 23:38:42 +0000

    Adding break so you don't get repeated outputs for day 6

--- Day6.py
+++ Day6.py
@@ -4,8 +4,10 @@ stream = open("Day6_Input.txt").read()
 for char_ind in range(4, len(stream)):
     if len(set(stream[char_ind-4:char_ind])) == 4:
         print("Part 1: "+str(char_ind))
+        break
 
 # -------- Part 2 ---------
 for char_ind in range(14, len(stream)):
     if len(set(stream[char_ind-14:char_ind])) == 14:
         print("Part 2: "+str(char_ind))
+        break
