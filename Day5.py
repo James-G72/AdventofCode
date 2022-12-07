6c9652c6a33e294ec1fc44bed7222d872dd86c63
James Gower <66065325+James-G72@users.noreply.github.com>
2022-12-07 08:37:55 +0000

    Day 5 part 1 complete, a lot of silly mistakes took a lot of debugging

---
+++ Day5.py
@@ -0,0 +1,35 @@
+import math
+
+info = open("Day5_Input.txt").read().split("\n")
+
+stack_row = []
+for x in info:
+    if x[0] == " " and x[1].isdigit():
+        stack_width = int(x[-1])
+        break
+    else:
+        stack_row.append(x)
+moves = info[len(stack_row)+2:]
+
+columns = [[] for x in range(0,9)]
+for stack in stack_row[::-1]:
+    char_ind = 0
+    while char_ind < len(stack):
+        if stack[char_ind] not in [" ","[","]"]:
+            columns[math.floor(char_ind/4)].append(stack[char_ind])
+        char_ind += 1
+
+for move in moves:
+    move_list = move.split(" ")
+    nums = [int(move_list[1]),int(move_list[3])-1,int(move_list[-1])-1]
+    for pick in range(0,nums[0]):
+        columns[nums[2]].append(columns[nums[1]][-1])
+        columns[nums[1]] = columns[nums[1]][:-1]
+
+p1_string = ""
+for column in columns:
+    if column != []:
+        p1_string += column[-1]
+    else:
+        p1_string += " "
+print("Part 1: "+p1_string)
