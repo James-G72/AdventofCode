76a8b08538c6521cb09a60085f89f8fcf45caa1a
James Gower <66065325+James-G72@users.noreply.github.com>
2023-12-03 20:05:38 +0000

    Day 3 correct with review  from Ruth

--- 2023/Day3.py
+++ 2023/Day3.py
@@ -22,34 +22,21 @@ def valid_numbers(num, sym1, sym2, sym3):
     num_list = []
     current_num = []
     valid = False
-    if num[0] != "":
-        current_num.append(num[0])
-        if valid_pos[0]:
-            valid = True
-    for pos in range(1, len(num)-1):
-        if num[pos] != "":
+    for pos in range(0, len(num)):
+        if num[pos].isdigit():
             current_num.append(num[pos])
             if valid_pos[pos]:
                 valid = True
         elif valid:
-            _ = ""
-            num_list.append(int("".join(map(str,current_num))))
+            num_list.append(int("".join(current_num)))
             current_num = []
             valid = False
         else:
             current_num = []
             # Don't think the statement below is entirely necessary but can't hurt
             valid = False
-    if num[-1] != "":
-        current_num.append(num[-1])
-        if valid_pos[-1]:
-            valid = True
-    elif valid:
-        _ = ""
-        num_list.append(int("".join(map(str,current_num))))
     if valid:
-        _ = ""
-        num_list.append(int("".join(map(str,current_num))))
+        num_list.append(int("".join(current_num)))
     print(num_list)
     return num_list
 
@@ -73,7 +60,7 @@ for pos in range(1, len(line_store)-1):
 number_store = [x * (x.isdigit()) for x in line_store[-1]]
 symbol_store1 = [x in SYMBOLS for x in line_store[-2]]
 symbol_store2 = [x in SYMBOLS for x in line_store[-1]]
-symbol_store3 = [False for _ in range(0, len(line_store[0]))]
+symbol_store3 = [False for _ in range(0, len(line_store[-1]))]
 total += sum(valid_numbers(number_store, symbol_store1, symbol_store2, symbol_store3))
 
 print("Sum of all valid numbers is: "+str(total))
