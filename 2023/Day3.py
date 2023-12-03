e96811afb31fe86a0f98e7364d00480a54ac6068
James Gower <66065325+James-G72@users.noreply.github.com>
2023-12-03 16:42:41 +0000

    First functioning Day3 which gives too high answer

--- 2023/Day3.py
+++ 2023/Day3.py
@@ -1,22 +1,75 @@
 
 SYMBOLS = ['%', '/', '-', '*', '$', '&', '+', '#', '=', '@']
 
+def valid_numbers(num, sym1, sym2, sym3):
+    """
+    Takes a line of numbers and three lines of symbols and returns all valid numbers as a list.
+    :param num: The position and value of numbers in the line
+    :param sym1: The symbols in the line above
+    :param sym2: The symbols in the line
+    :param sym3: The symbols in the line below
+    :return: num_list, all valid numbers in the schematic
+    """
+    valid_pos = [False for _ in range(len(num))]
+    if any(sym1[0:2]) or any(sym2[0:2]) or any(sym3[0:2]):
+        valid_pos[0] = True
+    for pos in range(1, len(num)):
+        if any(sym1[pos-1:pos+2]) or any(sym2[pos-1:pos+2]) or any(sym3[pos-1:pos+2]):
+            valid_pos[pos] = True
+    if any(sym1[-2:]) or any(sym2[-2:]) or any(sym3[-2:]):
+        valid_pos[0] = True
+
+    num_list = []
+    current_num = []
+    valid = False
+    if num[0] != "" and any(valid_pos[0:2]):
+        current_num.append(num[0])
+        valid = True
+    for pos in range(1, len(num)):
+        if num[pos] != "":
+            current_num.append(num[pos])
+            if any(valid_pos[pos-1:pos+2]):
+                valid = True
+        elif valid:
+            _ = ""
+            num_list.append(int("".join(map(str,current_num))))
+            current_num = []
+            valid = False
+        else:
+            current_num = []
+            # Don't think the statement below is entirely necessary but can't hurt
+            valid = False
+    if num[-1] != "" and any(valid_pos[-2:]):
+        current_num.append(num[0])
+        valid = True
+    elif valid:
+        _ = ""
+        num_list.append(int("".join(map(str,current_num))))
+
+    return num_list
+
 line_store = []
 for line in open("Day3_input.txt").read().split("\n"):
     line_store.append(line)
 
-number_store1 = [iter*(x.isdigit()) for iter, x in enumerate(line_store[0])]
-number_store2 = []
-number_store3 = []
-symbol_store1 = [iter*(x in SYMBOLS) for iter, x in enumerate(line_store[0])]
-symbol_store2 = []
-symbol_store3 = []
-width = len(line_store[0])
-for pos in range(1, len(line_store)):
-    symbol_store1 = [iter*(x in SYMBOLS) for iter, x in enumerate(line_store[pos-1])]
-    symbol_store2 = [iter * (x in SYMBOLS) for iter,x in enumerate(line_store[pos])]
-    symbol_store3 = [iter * (x in SYMBOLS) for iter,x in enumerate(line_store[pos+1])]
-    number_store1 = [iter*(x.isdigit()) for iter, x in enumerate(line_store[pos-1])]
-    number_store2 = [iter * (x.isdigit()) for iter,x in enumerate(line_store[pos])]
-    number_store3 = [iter * (x.isdigit()) for iter,x in enumerate(line_store[pos+2])]
-    t = 1
+total = 0
+number_store = [x * (x.isdigit()) for x in line_store[0]]
+symbol_store1 = [False for _ in range(0, len(line_store[0]))]
+symbol_store2 = [x in SYMBOLS for x in line_store[0]]
+symbol_store3 = [x in SYMBOLS for x in line_store[1]]
+# Perform calculation for the first line as a special case outside the loop
+total += sum(valid_numbers(number_store, symbol_store1, symbol_store2, symbol_store3))
+for pos in range(1, len(line_store)-1):
+    symbol_store1 = [x in SYMBOLS for x in line_store[pos-1]]
+    symbol_store2 = [x in SYMBOLS for x in line_store[pos]]
+    symbol_store3 = [x in SYMBOLS for x in line_store[pos+1]]
+    number_store = [x * (x.isdigit()) for x in line_store[pos]]
+    total += sum(valid_numbers(number_store, symbol_store1, symbol_store2, symbol_store3))
+number_store = [x * (x.isdigit()) for x in line_store[-1]]
+symbol_store1 = [x in SYMBOLS for x in line_store[-2]]
+symbol_store2 = [x in SYMBOLS for x in line_store[-1]]
+symbol_store3 = [False for _ in range(0, len(line_store[0]))]
+total += sum(valid_numbers(number_store, symbol_store1, symbol_store2, symbol_store3))
+
+print("Sum of all valid numbers is: "+str(total))
+
