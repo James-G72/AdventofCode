e5d98360a601193ccdf585fef64518998bf326fd
James Gower <66065325+James-G72@users.noreply.github.com>
2023-12-04 23:09:47 +0000

    Tidying the Day4 code

--- 2023/Day4.py
+++ 2023/Day4.py
@@ -1,7 +1,6 @@
-global TOTAL_CARDS
 
 total = 0
-for line in open("Day1_input.txt").read().split("\n"):
+for line in open("Day4_input.txt").read().split("\n"):
     winners_string, numbers_string = line.split(":")[1].split("|")
     winners_list = [x for x in winners_string.split(" ") if x != ""]
     numbers_list = [x for x in numbers_string.split(" ") if x != ""]
@@ -15,7 +14,7 @@ print("Total sum is: "+str(int(total)))
 
 # Part 2
 LINE_WINNERS = []
-for line in open("Day1_input.txt").read().split("\n"):
+for line in open("Day4_input.txt").read().split("\n"):
     winners_string, numbers_string = line.split(":")[1].split("|")
     winners_list = [x for x in winners_string.split(" ") if x != ""]
     numbers_list = [x for x in numbers_string.split(" ") if x != ""]
@@ -25,18 +24,15 @@ for line in open("Day1_input.txt").read().split("\n"):
             counter += 1
     LINE_WINNERS.append(counter)
 
-TOTAL_CARDS = 0
-
 def counting_points(card, count):
     count += 1
-    global TOTAL_CARDS
-    TOTAL_CARDS += 1
     if LINE_WINNERS[card] != 0:
         for x in range(0, LINE_WINNERS[card]):
             count = counting_points(card+x+1, count)
     return count
 
+card_num = 0
 for pos in range(0, len(LINE_WINNERS)):
-    counting_points(pos, 0)
+    card_num += counting_points(pos, 0)
 
-print("Total sum is: "+str(int(TOTAL_CARDS)))
+print("Total number of cards is: "+str(int(card_num)))
