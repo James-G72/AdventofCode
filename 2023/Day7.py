58458e8776149541e89206fa9b6998a239b7e5f9
James Gower <66065325+James-G72@users.noreply.github.com>
2023-12-06 08:18:13 +0000

    Day 6 part 2 (the file is called Day 7 because I made it as a txt file and cant revert that..)

--- 2023/Day7.py
+++ 2023/Day7.py
@@ -15,3 +15,18 @@ for idx, time in enumerate(times):
 print("Part 1 product of races is: "+str(races))
 
 # Part 2
+
+times = []
+records = []
+pos = 0
+for line in open("Day6_input.txt").read().split("\n"):
+    if pos == 0:
+        [times.append(x) for x in line.split(":")[1].split(" ") if x != ""]
+        pos = 1
+    else:
+        [records.append(x) for x in line.split(":")[1].split(" ") if x != ""]
+time = int("".join(times))
+record = int("".join(records))
+
+race = sum([(x*(time-x) > record) for x in range(0, time)])
+print("Part 2 winning ways is: "+str(race))
