abe0254e657d22e6b455b67541afbcac9e959d7b
James Gower <66065325+James-G72@users.noreply.github.com>
2023-12-07 21:30:10 +0000

    Totally re-done the code based on a tutorial of how to use .sort for the complexity of a list to create a sudo score

--- 2023/Day7.py
+++ 2023/Day7.py
@@ -1,35 +1,35 @@
-import pandas as pd
 
-hands = []
-bids = []
-for line in open("Day7_input.txt").read().split("\n"):
-    h, b = line.split(" ")
-    hands.append([char for char in h])
-    bids.append(b)
-
-cards = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"]
-order = [13-x for x in range(0, 14)]
-rank_dict = dict(zip(cards, order))
-
-strength = []
-strength = pd.Series()
-for idx, hand in enumerate(hands):
-    rank_count = [0]*14
+def hand_type(hand):
+    """
+    A function that iterates through the hand in order to establish which type of hand it is
+    :param hand: str, input of the hand to be considered
+    :return:
+    """
+    counts = {}
     for card in hand:
-        rank_count[rank_dict[card]] += 1
-    if max(rank_count) == 5:
-        strength._set_value(1000000000+rank_dict[hand[0]], "".join(hand))
-    elif max(rank_count) == 4:
-        main_card = [idx for idx, r in enumerate(rank_count) if r == max(rank_count)][0]
-        other_card = [rank_dict[c] for idx, c in enumerate(hand) if rank_dict[c] != main_card][0]
-        base = 1+main_card
-        possitional = [idx for idx, c in enumerate(hand) if rank_dict[c] != main_card][0]
-        stren = 100000000 + possitional*13 + other_card
-        strength._set_value(stren, "".join(hand))
-    # elif max(rank_count) == 3:
-    #
-    # elif max(rank_count) == 2:
-    #
-    # elif max(rank_count) == 1:
+        if card not in counts:
+            counts[card] = 0
+        counts[card] += 1
+    return sorted(counts.values(), reverse=True)
 
-print(strength.sort_index(ascending=False))
+def type_to_score(hand):
+    # Attempting to create a universal score
+    card_order = "23456789TJQKA"
+    mid = hand_type(hand)
+    mid.extend([card_order.index(x) for x in hand])
+    return mid
+
+scores = []
+for line in open("Day7_input.txt").read().split('\n'):
+    hand, bid = line.split()
+    score = type_to_score(hand)
+    scores.append([score, hand, int(bid)])
+# Essentially sorting by the length of the score
+scores.sort()
+
+total = 0
+for idx, hand_info in enumerate(scores):
+    bid = hand_info[2]
+    total += (idx+1) * bid
+
+print("Part 1: "+str(total))
