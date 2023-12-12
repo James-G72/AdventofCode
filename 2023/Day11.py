e4513c6cd886b9c3988e025c167eba22fdcd7ae8
James Gower <66065325+James-G72@users.noreply.github.com>
2023-12-12 01:20:22 +0000

    Day11 part 2 completed by counting each epty row or column 999999 times

--- 2023/Day11.py
+++ 2023/Day11.py
@@ -37,4 +37,55 @@ for galaxy_1 in galaxy_dixt.keys():
 
 print("Part 1: Distances = "+str(sum(distances.values())))
 
+# Part 2
+first = True
+lines = []
+for line in open("Day11_input.txt").read().split("\n"):
+    width = len(line)
+    lines.append(line)
+    if all([x == "." for x in line]):
+        lines.append("!"*width)
 
+pos = 0
+while pos < width:
+    if all([(x[pos] == ".") or (x[pos] == "!") for x in lines]):
+        for idx, line in enumerate(lines):
+            lines[idx] = line[:pos]+"!"+line[pos:]
+        pos += 2
+        width += 1
+    else:
+        pos += 1
+
+galaxy_dixt = {}
+count = 0
+for row, line in enumerate(lines):
+    for col, entry in enumerate(line):
+        if entry == "#":
+            galaxy_dixt["#"+str(count)] = [row, col]
+            count += 1
+
+mult = 999999
+distances = {}
+for galaxy_1 in galaxy_dixt.keys():
+    for galaxy_2 in galaxy_dixt.keys():
+        if galaxy_1 != galaxy_2 and (galaxy_1+":"+galaxy_2 not in distances.keys() and galaxy_2+":"+galaxy_1 not in distances.keys()):
+            loc_1 = galaxy_dixt[galaxy_1]
+            loc_2 = galaxy_dixt[galaxy_2]
+            horizontal = lines[loc_1[0]][min(loc_1[1],loc_2[1]):max(loc_1[1],loc_2[1])]
+            vertical = []
+            for line in lines[min(loc_1[0],loc_2[0]):max(loc_1[0],loc_2[0])]:
+                vertical.append(line[max(loc_1[1],loc_2[1])])
+            distance = 0
+            for x in horizontal:
+                if x == "!":
+                    distance += mult
+                else:
+                    distance += 1
+            for x in vertical:
+                if x == "!":
+                    distance += mult
+                else:
+                    distance += 1
+            distances[galaxy_1+":"+galaxy_2] = distance
+
+print("Part 2: Distances = "+str(sum(distances.values())))
